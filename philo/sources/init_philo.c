98b37553fdede5eda727d92e23e5742f245f0a45
Thea Wang <theawang@yahoo.com>
2023-06-08 16:01:05 +0200

    add: start init philo

--- philo/sources/init_philo.c
+++ philo/sources/init_philo.c
@@ -6,15 +6,44 @@
 /*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 15:05:22 by twang             #+#    #+#             */
-/*   Updated: 2023/06/08 13:00:45 by twang            ###   ########.fr       */
+/*   Updated: 2023/06/08 15:59:58 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	init_philosophers(t_data *data)
+/*---- prototypes ------------------------------------------------------------*/
+
+static int	_alloc_memory(t_data *data);
+static int	_init_mutex(t_data *data);
+
+/*----------------------------------------------------------------------------*/
+
+static int	_alloc_memory(t_data *data)
 {
-	if (alloc_memory(data) != 0)
+	data->table = (t_philo *)malloc(data->nb_of_philo) * sizeof(t_philo);
+	if (!data->table)
 		return (-1);
+	return (0);
+}
+
+static int	_init_mutex(t_data *data)
+{
+	int	i;
 	
+	i = -1;
+	pthread_mutex_init(&(data->m_write), NULL);
+	pthread_mutex_lock(&(data->m_write));
+	while (++i < data->nb_of_philo)
+		launch_one_philo(data, i);
+	pthread_mutex_unlock(&(data->m_write));
+}
+
+int	init_philosophers(t_data *data)
+{
+	if (_alloc_memory(data) != 0)
+		return (-1);
+	if (_init_mutex != 0)
+		return (-1);
+	return (0);
 }
